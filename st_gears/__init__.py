c5dd2647db46debfb34f1e8c24e3d0904cc5225c
Tianyi-Summer <96898334+Tianyi-Summer@users.noreply.github.com>
2024-04-26 17:39:25 +0800

    Update __init__.py

--- st_gears/__init__.py
+++ st_gears/__init__.py
@@ -4,3 +4,7 @@ from .recons import stack_slices_pairwise_rigid
 from .recons import stack_slices_pairwise_elas_field
 
 from .visual import RegisPlotter
+
+from .granularity_adjusting import binning, interpolate
+
+from .calculate_max_kl import calculate_max_kl
